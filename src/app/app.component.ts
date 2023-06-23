fcd57d7bda610049c2d3636ac3235f6041e9acc8
engueranraout <rt.engueran@gmail.com>
2023-06-23 23:22:29 +0200

    Console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortefolioConsole';
+
+
+  ngOnInit() {
+      this.changeGradient();
+  }
+
+  public changeGradient() {
+    const color1 = this.getVibrantColor();
+    const color2 = this.getPastelColor();
+    const angle = Math.floor(Math.random() * 360);
+    const backgroundElement = document.getElementById('background');
+    if (backgroundElement) {
+      backgroundElement.style.background = `linear-gradient(${angle}deg, ${color1}, ${color2})`;
+    }
+  }
+
+  getVibrantColor() {
+    const vibrantColors = ['red', 'blue', 'magenta', 'pink', 'skyblue', 'lavender'];
+    return vibrantColors[Math.floor(Math.random() * vibrantColors.length)];
+  }
+
+  getPastelColor() {
+    return "#" + Math.random().toString(16).slice(2, 8).toUpperCase() + "80";
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { WindowComponent } from './window/window.component';
+import { NavigationWindowComponent } from './navigation-window/navigation-window.component';
+import { ConsoleComponent } from './console/console.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent,
+    NavigationWindowComponent,
+    ConsoleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
